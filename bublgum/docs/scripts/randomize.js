fa903fbc8cc8804980667af31f034a2b18330bd1
bublgumcomics <SomeGuyofelse@gmail.com>
2024-06-21 17:21:14 -0400

    New structure

---
+++ bublgum/docs/scripts/randomize.js
@@ -0,0 +1,7 @@
+function scrollToRandomComic() {
+    const comics = document.querySelectorAll('.comic-section');
+    const randomIndex = Math.floor(Math.random() * comics.length);
+    const randomComic = comics[randomIndex];
+
+    randomComic.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
